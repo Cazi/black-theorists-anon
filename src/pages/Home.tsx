9bd09ee658e5533ca0edff6e5cb7db68bf08633a
Cazi <71891696+Cazi@users.noreply.github.com>
2023-01-30 22:28:14 -0500

    Fixed circle

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import '../styles.css'
+import { motion } from "framer-motion";
+
 const home = () => {
   return (
     <div className='home'>
-        
+      <div>
+        <motion.img
+          className="box"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, y: -100, scale: 1 }}
+          transition={{
+            repeatType: "reverse",
+            repeat: Infinity,
+            repeatDelay: 1.5,
+            duration: 0.5,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01]
+          }}
+        />
+      </div>
     </div>
   )
 }
