5d9a9707e0afc3203c9becb2a813ac0833b7ea57
Cazi <71891696+Cazi@users.noreply.github.com>
2023-02-08 12:34:20 -0500

    Worked on Navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,21 +5,25 @@ import { motion } from "framer-motion";
 const home = () => {
   return (
     <div className='home'>
-      <div>
-        <motion.img
-          className="box"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, y: -100, scale: 1 }}
-          transition={{
-            repeatType: "reverse",
-            repeat: Infinity,
-            repeatDelay: 1.5,
-            duration: 0.5,
-            delay: 0.5,
-            ease: [0, 0.71, 0.2, 1.01]
-          }}
-        />
+      <div className="navbar">
+        <div className="navbar-left">
+          Black Theorists Anonymous
+        </div>
+        <div className="navbar-right">
+          <div className="about"> Who We Are</div>
+          <div className="activities">What We Do</div>
+        </div>
       </div>
+      {/* <div>
+        <motion.button
+          className="box"
+          initial={{ opacity: 0, y: 50, scale: 0.5 }}
+          animate={{ opacity: 1, y: -100, scale: 1 }}
+          
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.5 }}
+        />
+      </div> */}
     </div>
   )
 }
