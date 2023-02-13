87279dffc58714b7bfc1c58a0fabff74cc722f5b
Cazi <71891696+Cazi@users.noreply.github.com>
2023-02-13 15:57:53 -0500

    Started about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,41 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 import Navbar from '../components/Navbar'
 import '../styles/style-about.css'
 const About = () => {
   return (
     <div className='about'>
-        <Navbar/>
-        <p>About Us</p>
-    </div>
+      <Navbar />
+      <h1 className='about-head'>About Us</h1>
+      <div className='personas'>
+
+        <motion.button
+          className="box"
+          initial={{ opacity: 0, y: 50, scale: 0.5 }}
+          animate={{ opacity: 1, y: -100, scale: 1 }}
+
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.5 }}
+        />
+
+        <motion.button
+          className="box"
+          initial={{ opacity: 0, y: 50, scale: 0.5 }}
+          animate={{ opacity: 1, y: -100, scale: 1 }}
+
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.5 }}
+        />
+        <motion.button
+          className="box"
+          initial={{ opacity: 0, y: 50, scale: 0.5 }}
+          animate={{ opacity: 1, y: -100, scale: 1 }}
+
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.5 }}
+        />
+      </div >
+    </div >
   )
 }
 
